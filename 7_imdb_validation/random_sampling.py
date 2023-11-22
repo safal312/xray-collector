0596d915f09cc2d87593c3ebd810037d7feafa7f
yeonieheoo <sh5874@nyu.edu>
2023-11-22 16:16:20 +0400

    randomized samples by batches

---
+++ 7_imdb_validation/random_sampling.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import random
+
+# Load the CSV file into a DataFrame
+df = pd.read_csv('metadata_for_validation.csv')
+
+# Create a dictionary to store DataFrames for each 'dir' category
+dir_dfs = {}
+
+# Iterate over unique 'dir' categories
+for directory in df['dir'].unique():
+    # Select rows for the current 'dir' category
+    dir_rows = df[df['dir'] == directory]
+    
+    # Randomly sample 30 rows
+    sampled_rows = dir_rows.sample(n=min(30, len(dir_rows)), random_state=42)
+    
+    # Store the sampled DataFrame in the dictionary
+    dir_dfs[directory] = sampled_rows
+
+# Save each DataFrame to a separate CSV file
+for directory, sampled_df in dir_dfs.items():
+    file_name = f'{directory}_sampled.csv'
+    sampled_df.to_csv(file_name, index=False)
+
+print("Sampling and saving completed.")
